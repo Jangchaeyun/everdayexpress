86cb09bb0ff465713b5dde00f226e147cd35ee84
Jangchaeyun <zzangsally@naver.com>
2024-10-22 22:56:04 +0900

    1022 SpringBoot Create

---
+++ everydayexpress/src/main/java/com/everyday/EverydayexpressApplication.java
@@ -0,0 +1,13 @@
+package com.everyday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverydayexpressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverydayexpressApplication.class, args);
+    }
+
+}
