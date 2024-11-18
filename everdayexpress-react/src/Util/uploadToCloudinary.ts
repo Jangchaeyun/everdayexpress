69ff689317a55fd5d32fab9fb891cb1cc4127b68
Jangchaeyun <zzangsally@naver.com>
2024-11-18 22:36:59 +0900

    1118 AddProduct

---
+++ everdayexpress-react/src/Util/uploadToCloudinary.ts
@@ -0,0 +1,24 @@
+export const uploadToCloudinary = async (pics: any) => {
+  const cloud_name = "dmxnml9p1";
+  const upload_preset = "myeverydayexpress";
+
+  if (pics) {
+    const data = new FormData();
+    data.append("file", pics);
+    data.append("upload_preset", upload_preset);
+    data.append("clound_name", cloud_name);
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/dmxnml9p1/upload",
+      {
+        method: "POST",
+        body: data,
+      }
+    );
+
+    const fileDate = await res.json();
+    return fileDate.url;
+  } else {
+    console.log("error : pics not found");
+  }
+};
--- everdayexpress-react/src/seller/pages/Products/AddProduct.tsx
+++ everdayexpress-react/src/seller/pages/Products/AddProduct.tsx
@@ -1,7 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+import { menLevelTwo } from "../../../data/category/level two/menLevelTwo";
+import { womenLevelTwo } from "../../../data/category/level two/womenLevelTwo";
+import { useFormik } from "formik";
+import { uploadToCloudinary } from "../../../Util/uploadToCloudinary";
+import { CircularProgress, Grid, Grid2, IconButton } from "@mui/material";
+import { AddPhotoAlternate } from "@mui/icons-material";
+
+const categoryTwo: { [key: string]: any[] } = {
+  men: menLevelTwo,
+  women: womenLevelTwo,
+};
+
+const categoryThree: { [key: string]: any[] } = {};
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  const [uploadImage, setUploadingImage] = useState(false);
+  const [snackbarOpen, setOpenSnackbar] = useState(false);
+
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      description: "",
+      mrpPrice: "",
+      sellingPrice: "",
+      quantity: "",
+      color: "",
+      images: [],
+      category: "",
+      category2: "",
+      category3: "",
+      sizes: "",
+    },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const handleImageChange = async (event: any) => {
+    const file = event.target.files[0];
+    setUploadingImage(true);
+    const image = await uploadToCloudinary(file);
+    formik.setFieldValue("images", [...formik.values.images, image]);
+    setUploadingImage(false);
+  };
+
+  const handleRemoveImage = (index: number) => {
+    const updatedImages = [...formik.values.images];
+    updatedImages.splice(index, 1);
+    formik.setFieldValue("images", updatedImages);
+  };
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit} className="space-y-4 p-4">
+        <Grid2 container spacing={2}>
+          <Grid2 className="flex flex-wrap gap-5" size={{ xs: 12 }}>
+            <input
+              type="file"
+              accept="image/*"
+              id="fileInput"
+              style={{ display: "none" }}
+              onChange={handleImageChange}
+            />
+            <label className="relative" htmlFor="fileImput">
+              <span className="w-24 h-24 cursor-pointer flex items-center justify-center p-3 border rounded-md border-gray-400">
+                <AddPhotoAlternate className="text-gray-700" />
+              </span>
+              {uploadImage && (
+                <div className="absolute left-0 right-0 top-0 bottom-0 w-24 h-24 flex justify-center items-center">
+                  <CircularProgress />
+                </div>
+              )}
+            </label>
+
+            <div className="flex flex-wrap gap-2">
+              {formik.values.images.map((image, index) => (
+                <div className="relative">
+                  <img
+                    className="w-24 h-24 object-cover"
+                    key={index}
+                    src={image}
+                    alt={`ProductImage ${index + 1}`}
+                  />
+                  {/* <IconButton
+                    omClick={() => handleRemoveImage(index)}
+                    className=""
+                    size="small"
+                    color="error"
+                    sx={{
+                      position: "absolute",
+                      top: 0,
+                      right: 0,
+                      outline: "none",
+                    }}
+                  ></IconButton> */}
+                </div>
+              ))}
+            </div>
+          </Grid2>
+        </Grid2>
+      </form>
+    </div>
+  );
 };
 
 export default AddProduct;
