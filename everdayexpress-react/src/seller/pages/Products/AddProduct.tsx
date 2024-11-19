2709c729b7566722efeaf90ff8784cbf1a2b1be2
Jangchaeyun <zzangsally@naver.com>
2024-11-19 21:31:52 +0900

    1119 AddProduct

--- everdayexpress-react/src/seller/pages/Products/AddProduct.tsx
+++ everdayexpress-react/src/seller/pages/Products/AddProduct.tsx
@@ -3,15 +3,42 @@ import { menLevelTwo } from "../../../data/category/level two/menLevelTwo";
 import { womenLevelTwo } from "../../../data/category/level two/womenLevelTwo";
 import { useFormik } from "formik";
 import { uploadToCloudinary } from "../../../Util/uploadToCloudinary";
-import { CircularProgress, Grid, Grid2, IconButton } from "@mui/material";
-import { AddPhotoAlternate } from "@mui/icons-material";
+import {
+  Button,
+  CircularProgress,
+  FormControl,
+  FormHelperText,
+  Grid,
+  Grid2,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import {
+  AddPhotoAlternate,
+  Close,
+  FormatLineSpacing,
+} from "@mui/icons-material";
+import { colors } from "../../../data/Filter/color";
+import { mainCategory } from "../../../data/category/mainCategory";
+import { allLevelTwo } from "../../../data/category/level two/allLevelTwo";
+import { allLevelThree } from "../../../data/category/level tree/allLevelThree";
+import { menLevelThree } from "../../../data/category/level tree/menLevelThree";
+import { womenLevelThree } from "../../../data/category/level tree/womenLevelThree";
 
 const categoryTwo: { [key: string]: any[] } = {
+  all: allLevelTwo,
   men: menLevelTwo,
   women: womenLevelTwo,
 };
 
-const categoryThree: { [key: string]: any[] } = {};
+const categoryThree: { [key: string]: any[] } = {
+  all: allLevelThree,
+  men: menLevelThree,
+  women: womenLevelThree,
+};
 
 const AddProduct = () => {
   const [uploadImage, setUploadingImage] = useState(false);
@@ -40,6 +67,7 @@ const AddProduct = () => {
     const file = event.target.files[0];
     setUploadingImage(true);
     const image = await uploadToCloudinary(file);
+
     formik.setFieldValue("images", [...formik.values.images, image]);
     setUploadingImage(false);
   };
@@ -48,6 +76,15 @@ const AddProduct = () => {
     const updatedImages = [...formik.values.images];
     updatedImages.splice(index, 1);
     formik.setFieldValue("images", updatedImages);
+  };
+
+  const childCategory = (category: any, parentCategoryId: any) => {
+    return category.filter((child: any) => {
+      return child.parentCategoryId == parentCategoryId;
+    });
+  };
+  const handleCloseSnackbar = () => {
+    setOpenSnackbar(false);
   };
   return (
     <div>
@@ -61,7 +98,7 @@ const AddProduct = () => {
               style={{ display: "none" }}
               onChange={handleImageChange}
             />
-            <label className="relative" htmlFor="fileImput">
+            <label className="relative" htmlFor="fileInput">
               <span className="w-24 h-24 cursor-pointer flex items-center justify-center p-3 border rounded-md border-gray-400">
                 <AddPhotoAlternate className="text-gray-700" />
               </span>
@@ -81,8 +118,8 @@ const AddProduct = () => {
                     src={image}
                     alt={`ProductImage ${index + 1}`}
                   />
-                  {/* <IconButton
-                    omClick={() => handleRemoveImage(index)}
+                  <IconButton
+                    onClick={() => handleRemoveImage(index)}
                     className=""
                     size="small"
                     color="error"
@@ -92,10 +129,239 @@ const AddProduct = () => {
                       right: 0,
                       outline: "none",
                     }}
-                  ></IconButton> */}
+                  >
+                    <Close sx={{ fonstSize: "1rem" }} />
+                  </IconButton>
                 </div>
               ))}
             </div>
+          </Grid2>
+          <Grid2 size={{ xs: 12 }}>
+            <TextField
+              fullWidth
+              id="title"
+              name="title"
+              label="이름"
+              value={formik.values.title}
+              onChange={formik.handleChange}
+              error={formik.touched.title && Boolean(formik.errors.title)}
+              helperText={formik.touched.title && formik.errors.title}
+              required
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12 }}>
+            <TextField
+              multiline
+              rows={4}
+              fullWidth
+              id="description"
+              name="description"
+              label="설명"
+              value={formik.values.description}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.description && Boolean(formik.errors.description)
+              }
+              helperText={
+                formik.touched.description && formik.errors.description
+              }
+              required
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 4, lg: 3 }}>
+            <TextField
+              fullWidth
+              id="mrp_price"
+              name="mrpPrice"
+              label="현재 가격"
+              type="number"
+              value={formik.values.mrpPrice}
+              onChange={formik.handleChange}
+              error={formik.touched.mrpPrice && Boolean(formik.errors.mrpPrice)}
+              helperText={formik.touched.mrpPrice && formik.errors.mrpPrice}
+              required
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 4, lg: 3 }}>
+            <TextField
+              fullWidth
+              id="selling_price"
+              name="sellingPrice"
+              label="할인 가격"
+              type="number"
+              value={formik.values.sellingPrice}
+              onChange={formik.handleChange}
+              error={
+                formik.touched.sellingPrice &&
+                Boolean(formik.errors.sellingPrice)
+              }
+              helperText={
+                formik.touched.sellingPrice && formik.errors.sellingPrice
+              }
+              required
+            />
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 4, lg: 3 }}>
+            <FormControl
+              fullWidth
+              error={formik.touched.color && Boolean(formik.errors.color)}
+              required
+            >
+              <InputLabel id="color-label">색상</InputLabel>
+              <Select
+                labelId="color-label"
+                id="color"
+                name="color"
+                value={formik.values.color}
+                onChange={formik.handleChange}
+                label="색상"
+              >
+                <MenuItem value="">
+                  <em>없음</em>
+                </MenuItem>
+                {colors.map((color, index) => (
+                  <MenuItem value={color.name}>
+                    <div className="flex gap-3">
+                      <span
+                        style={{ backgroundColor: color.hex }}
+                        className={`h-5 w-5 rounded-full ${
+                          color.name === "화이트" ? "border" : ""
+                        }`}
+                      ></span>
+                      <p>{color.name}</p>
+                    </div>
+                  </MenuItem>
+                ))}
+              </Select>
+              {formik.touched.color && formik.errors.color && (
+                <FormHelperText>{formik.errors.color}</FormHelperText>
+              )}
+            </FormControl>
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 4, lg: 3 }}>
+            <FormControl
+              fullWidth
+              error={formik.touched.sizes && Boolean(formik.errors.sizes)}
+              required
+            >
+              <InputLabel id="sizes-label">사이즈</InputLabel>
+              <Select
+                labelId="sizes-label"
+                id="sizes"
+                name="sizes"
+                value={formik.values.sizes}
+                onChange={formik.handleChange}
+                label="사이즈"
+              >
+                <MenuItem value="">
+                  <em>없음</em>
+                </MenuItem>
+                <MenuItem value="FREE">프리</MenuItem>
+                <MenuItem value="S">S</MenuItem>
+                <MenuItem value="M">M</MenuItem>
+                <MenuItem value="L">L</MenuItem>
+                <MenuItem value="XL">XL</MenuItem>
+              </Select>
+              {formik.touched.sizes && formik.errors.sizes && (
+                <FormHelperText>{formik.errors.sizes}</FormHelperText>
+              )}
+            </FormControl>
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 4, lg: 4 }}>
+            <FormControl
+              fullWidth
+              error={formik.touched.category && Boolean(formik.errors.category)}
+              required
+            >
+              <InputLabel id="category-label">카테고리</InputLabel>
+              <Select
+                labelId="category-label"
+                id="category"
+                name="category"
+                value={formik.values.category}
+                onChange={formik.handleChange}
+                label="카테고리"
+              >
+                {mainCategory.map((item) => (
+                  <MenuItem value={item.categoryId}>{item.name}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 4, lg: 4 }}>
+            <FormControl
+              fullWidth
+              error={
+                formik.touched.category2 && Boolean(formik.errors.category2)
+              }
+              required
+            >
+              <InputLabel id="category2-label">2번째 카테고리</InputLabel>
+              <Select
+                labelId="category2-label"
+                id="category"
+                name="category2"
+                value={formik.values.category2}
+                onChange={formik.handleChange}
+                label="카테고리2"
+              >
+                {formik.values.category &&
+                  categoryTwo[formik.values.category]?.map((item) => (
+                    <MenuItem value={item.categoryId}>{item.name}</MenuItem>
+                  ))}
+              </Select>
+            </FormControl>
+          </Grid2>
+          <Grid2 size={{ xs: 12, md: 4, lg: 4 }}>
+            <FormControl
+              fullWidth
+              error={
+                formik.touched.category3 && Boolean(formik.errors.category3)
+              }
+              required
+            >
+              <InputLabel id="category-label">3번째 카테고리</InputLabel>
+              <Select
+                labelId="category-label"
+                id="category"
+                name="category3"
+                value={formik.values.category3}
+                onChange={formik.handleChange}
+                label="3번째 카테고리"
+              >
+                <MenuItem value="">
+                  <em>없음</em>
+                </MenuItem>
+                {formik.values.category2 &&
+                  childCategory(
+                    categoryThree[formik.values.category],
+                    formik.values.category2
+                  )?.map((item: any) => (
+                    <MenuItem value={item.categoryId}>{item.name}</MenuItem>
+                  ))}
+              </Select>
+              {formik.touched.category && formik.errors.category && (
+                <FormHelperText>{formik.errors.category}</FormHelperText>
+              )}
+            </FormControl>
+          </Grid2>
+          <Grid2 size={{ xs: 12 }}>
+            <Button
+              sx={{ p: "14px" }}
+              color="primary"
+              variant="contained"
+              fullWidth
+              type="submit"
+            >
+              {false ? (
+                <CircularProgress
+                  size="small"
+                  sx={{ width: "27px", height: "27px" }}
+                />
+              ) : (
+                "제품 추가"
+              )}
+            </Button>
           </Grid2>
         </Grid2>
       </form>
