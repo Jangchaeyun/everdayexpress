0b17fce9c1cbb13908ecaed60a25d62a0d4de8cc
Jangchaeyun <zzangsally@naver.com>
2024-11-25 18:56:52 +0900

    1125 SellerDashBoard Front & Back Connect

--- everdayexpress-react/src/component/DrawerList.tsx
+++ everdayexpress-react/src/component/DrawerList.tsx
@@ -1,6 +1,8 @@
 import { Divider, ListItemIcon, ListItemText } from "@mui/material";
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../State/Store";
+import { logout } from "../State/AuthSlice";
 
 interface menuItem {
   name: string;
@@ -18,6 +20,11 @@ interface DrawerListProps {
 const DrawerList = ({ menu, menu2, toggleDrawer }: DrawerListProps) => {
   const location = useLocation();
   const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout(navigate));
+  };
   return (
     <div className="h-full">
       <div className="flex flex-col justify-between h-full w-[300px] border-r py-5">
@@ -47,7 +54,10 @@ const DrawerList = ({ menu, menu2, toggleDrawer }: DrawerListProps) => {
         <div className="space-y-2">
           {menu2.map((item, index: number) => (
             <div
-              onClick={() => navigate(item.path)}
+              onClick={() => {
+                navigate(item.path);
+                if (item.path == "/") handleLogout();
+              }}
               className="pr-9 cursor-pointer"
               key={index}
             >
